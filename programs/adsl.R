87b38ba2d79cc3626f3df881354b03d4edb527a7
Stavros Oikonomou <stavoikono@gmail.com>
2023-01-31 18:33:10 +0000

    adding more ADSL variables

--- programs/adsl.R
+++ programs/adsl.R
@@ -1,15 +1,21 @@
-library(haven)
-library(admiral)
-library(dplyr)
-library(tidyr)
-library(metacore)
-library(metatools)
-library(xportr)
-library(stringr)
+# Loading the libraries ----
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
 
+packages <- c("haven","admiral","dplyr","tidyr","metacore","metatools","xportr","stringr")
+
+ipak(packages)
+
+
+# Loading SDTM ----
 dm <- read_xpt("sdtm/dm.xpt")
 ex <- read_xpt("sdtm/ex.xpt")
 vs <- read_xpt("sdtm/vs.xpt")
+ds <- read_xpt("sdtm/ds.xpt")
 
 # specs <- readxl::read_excel("metadata/specs.xlsx",
 #                     sheet = "Variables") %>%
@@ -21,6 +27,8 @@ vs <- read_xpt("sdtm/vs.xpt")
 
 
 #xportr_write(adsl, "adam/adsl.xpt")
+
+# Deriving ADSL ----
 
 ex_ext <- ex %>%
   derive_vars_dtm(
@@ -72,7 +80,11 @@ adsl <- adsl %>%
 
 adsl <- adsl %>%
   derive_vars_dtm_to_dt(source_vars = vars(TRTSDTM, TRTEDTM)) %>%
-  derive_var_trtdurd()
+  derive_var_trtdurd() %>%
+  derive_vars_dt(
+    dtc = RFENDTC,
+    new_vars_prefix = "RFEN"
+  )
 
 adsl <- adsl %>%
   mutate(TRT01AN = case_when(TRT01A=="Placebo" ~ 1,
@@ -96,4 +108,33 @@ adsl <- adsl %>%
          BMIBLGR1 = case_when(BMIBL < 25 ~ "<25",
                               BMIBL >=25 & BMIBL < 30 ~ "25-<30",
                               BMIBL >=30 ~ ">=30"))
+
+
+adsl <- adsl %>%
+  mutate(RACEN = case_when(RACE=="AMERICAN INDIAN OR ALASKA NATIVE" ~ 1,
+                           RACE=="ASIAN" ~ 2,
+                           RACE=="BLACK OR AFRICAN AMERICAN" ~ 3,
+                           RACE=="WHITE" ~ 6)) %>%
+  mutate(ITTFL = ifelse(!is.na(ARMCD),"Y","N")) %>%
+  mutate(SAFFL = ifelse(ITTFL == "Y" & !is.na(TRTSDT),"Y","N"))
+
+
+vs_8 <- vs %>% filter(VISITNUM==8) %>% distinct(USUBJID, VSDTC) %>% dplyr::rename(VSDTC8 = VSDTC)
+vs_10 <- vs %>% filter(VISITNUM==10) %>% distinct(USUBJID, VSDTC) %>% dplyr::rename(VSDTC10 = VSDTC)
+vs_12 <- vs %>% filter(VISITNUM==12) %>% distinct(USUBJID, VSDTC) %>% dplyr::rename(VSDTC12 = VSDTC)
+
+adsl <- adsl %>%
+  left_join(vs_8) %>%
+  left_join(vs_10) %>%
+  left_join(vs_12) %>%
+  mutate(COMP8FL = ifelse(!is.na(VSDTC8) & VSDTC8<= RFENDTC,"Y","N"),
+         COMP16FL = ifelse(!is.na(VSDTC10) & VSDTC10<= RFENDTC,"Y","N"),
+         COMP24FL = ifelse(!is.na(VSDTC12) & VSDTC12<= RFENDTC,"Y","N")) %>%
+  select(-VSDTC8,-VSDTC10,-VSDTC12)
+
+
+adsl <- adsl %>%
+  left_join(ds %>% filter(DSCAT=="DISPOSITION EVENT") %>% select(USUBJID,DSDECOD)) %>%
+  mutate(EOSSTT=ifelse(DSDECOD=="COMPLETED","COMPLETED","DISCONTINUED"))
+
 
