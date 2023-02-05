d76b8ca49a600f858314f800820ca43a70892bdd
Stavros Oikonomou <stavoikono@gmail.com>
2023-02-05 10:02:59 +0000

    adding cumdose

--- programs/adsl.R
+++ programs/adsl.R
@@ -6,7 +6,7 @@ ipak <- function(pkg){
   sapply(pkg, require, character.only = TRUE)
 }
 
-packages <- c("haven","admiral","dplyr","tidyr","metacore","metatools","xportr","stringr","readxl")
+packages <- c("haven","admiral","dplyr","tidyr","metacore","metatools","xportr","stringr","readxl","labelled")
 
 ipak(packages)
 
@@ -31,21 +31,27 @@ trtn <- function(x){
 }
 
 comp_func <- function(x){
-  ifelse(!is.na(x),"Y","N")
+  if_else(!is.na(x),"Y","N")
 }
 
 format_eosstt <- function(DSDECOD) {
-  ifelse(DSDECOD=="COMPLETED","COMPLETED","DISCONTINUED")
+  if_else(DSDECOD=="COMPLETED","COMPLETED","DISCONTINUED")
 }
 
 # Deriving ADSL ----
+
+pooled_sites_id <- dm %>%
+  filter(ARM!="Screen Failure") %>%
+  count(SITEID, sort = T) %>%
+  filter(n<3) %>%
+  pull(SITEID)
 
 adsl <- dm %>%
   filter(ARM!="Screen Failure") %>%
   select(-DOMAIN) %>%
   mutate(TRT01P = ARM,
          TRT01A = TRT01P,
-         SITEGR1 = SITEID,
+         SITEGR1 = if_else(SITEID %in% pooled_sites_id,"900",SITEID),
          AGEGR1 = case_when(AGE < 65 ~ "<65",
                             AGE >=65 & AGE<=80 ~ "65-80",
                             TRUE ~ ">80",),
@@ -163,8 +169,8 @@ adsl <- adsl %>%
     new_vars = vars(DSDECOD),
     filter_add = DSCAT == "DISPOSITION EVENT"
   ) %>%
-  mutate(DISCONFL = ifelse(DCSREAS!="COMPLETED","Y",NA_character_),
-         DSRAEFL = ifelse(DCSREAS!="ADVERSE EVENT","Y",NA_character_)) %>%
+  mutate(DISCONFL = if_else(DCSREAS!="COMPLETED","Y",NA_character_),
+         DSRAEFL = if_else(DCSREAS!="ADVERSE EVENT","Y",NA_character_)) %>%
   derive_vars_merged(
     dataset_add = mh,
     by_vars = vars(USUBJID),
@@ -228,8 +234,8 @@ efffl <- qs %>%
 
 
 adsl <- adsl %>%
-  mutate(ITTFL = ifelse(!is.na(ARMCD),"Y","N"),
-         SAFFL = ifelse(ITTFL == "Y" & !is.na(TRTSDT),"Y","N")
+  mutate(ITTFL = if_else(!is.na(ARMCD),"Y","N"),
+         SAFFL = if_else(ITTFL == "Y" & !is.na(TRTSDT),"Y","N")
   ) %>%
   left_join(efffl) %>%
   mutate(efffl_fl = if_else(is.na(efffl_fl),"N","Y")) %>%
@@ -238,8 +244,47 @@ adsl <- adsl %>%
 
 rm(efffl)
 
+## Deriving CUMDOSE
 
-# Export ADSL ----
+test <- adsl %>%
+  derive_vars_merged(
+    dataset_add = sv,
+    by_vars = vars(STUDYID, USUBJID),
+    new_vars = vars(VISIT4DT = SVSTDTC),
+    filter_add = VISITNUM==4
+  ) %>%
+  derive_vars_merged(
+    dataset_add = sv,
+    by_vars = vars(STUDYID, USUBJID),
+    new_vars = vars(VISIT12DT = SVSTDTC),
+    filter_add = VISITNUM==12
+  ) %>%
+  left_join(
+    vs %>%
+      filter(VISITNUM!=201) %>%
+      group_by(USUBJID) %>%
+      slice_max(VISITNUM) %>%
+      ungroup() %>%
+      select(USUBJID,VISITNUM,VSDTC) %>%
+      distinct()
+  ) %>%
+  select(USUBJID, TRT01PN, VISITNUM, TRTSDT, VISIT4DT, VISIT12DT, TRTEDT) %>%
+  filter(TRT01PN==2) %>%
+  mutate(VISIT4DT = as.Date(VISIT4DT),
+         VISIT12DT = as.Date(VISIT12DT)) %>%
+  mutate(VISIT4DT = if_else(VISITNUM<=4 & VISITNUM>3,TRTEDT,VISIT4DT),
+         VISIT12DT = if_else(VISITNUM<=12 & VISITNUM>4,TRTEDT,VISIT12DT)) %>%
+  mutate(INTERVAL1 = if_else(VISITNUM>3,as.numeric(difftime(VISIT4DT,TRTSDT,units = "days")) + 1,0),
+         INTERVAL2 = if_else(VISITNUM>4, as.numeric(difftime(VISIT12DT,VISIT4DT,units = "days")), 0),
+         INTERVAL3 = if_else(VISITNUM>12, as.numeric(difftime(TRTEDT, VISIT12DT,units = "days")), 0)) %>%
+  mutate(CUMDOSE = 54*INTERVAL1 + 81*INTERVAL2 + 54*INTERVAL3) %>%
+  select(USUBJID,TRT01PN,CUMDOSE)
+
+
+
+# Formatting ADSL for extraction ----
+
+## Ordering ----
 
 var_order <- read_excel("metadata/specs.xlsx",
                     sheet = "Variables") %>%
@@ -248,5 +293,16 @@ var_order <- read_excel("metadata/specs.xlsx",
 
 adsl <- adsl %>%
   select(all_of(var_order))
+
+## Adding labels for derived variables ----
+
+labels <- read_excel("metadata/specs.xlsx",
+                     sheet = "Variables") %>%
+  filter(Dataset=="ADSL") %>%
+  pull(Label)
+
+adsl <- set_variable_labels(adsl, .labels = labels)
+
+# Export ADSL ----
 
 xportr_write(adsl, "adam/adsl.xpt")
