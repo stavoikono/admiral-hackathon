ec91f454b4c3ee1fef2ab3bbbc016442b7bf524a
Stavros Oikonomou <stavoikono@gmail.com>
2023-02-09 22:17:13 +0000

    updating ADAE

--- programs/adae.R
+++ programs/adae.R
@@ -35,4 +35,36 @@ adae <- ae %>%
   derive_vars_dt(
     new_vars_prefix = "AEN",
     dtc = AEENDTC
-  )
+  ) %>%
+  derive_vars_dt(
+    new_vars_prefix = "AST",
+    dtc = AESTDTC,
+    highest_imputation = "D",
+    min_dates = vars(TRTSDT)
+  ) %>%
+  rowwise() %>%
+  mutate(
+    ADURN = as.numeric(AENDT - ASTDT + 1)
+  ) %>%
+  ungroup() %>%
+  mutate(ADURU = if_else(!is.na(ADURN),"DAY", NA_character_)) %>%
+  derive_vars_dy(
+    reference_date = TRTSDT,
+    source_vars = vars(TRTSDT, AENDT, ASTDT)
+  ) %>%
+  mutate(TRTEMFL2 = case_when(is.na(ASTDT) ~ "N",
+                              ASTDT >= TRTSDT ~"Y",
+                              TRUE ~ "N")
+  ) %>%
+  #derive_var_trtemfl(new_var = TRTEMFL,start_date = ASTDT, end_date = AENDT, trt_start_date = TRTSDT)
+  mutate(
+    CQ01NAM =
+      if_else(
+        (str_detect(AEDECOD,paste0(c("APPLICATION","DERMATITIS", "ERYTHEMA", "BLISTER"),collapse = "|")) |
+          (AEBODSYS=="SKIN AND SUBCUTANEOUS TISSUE DISORDERS")) &
+          !AEDECOD %in% c('COLD SWEAT', 'HYPERHIDROSIS', 'ALOPECIA'),
+        'DERMATOLOGIC EVENTS',NA_character_
+  ))
+
+
+
